06a254528e51e836fe38cad1d79e5b68d117b1cb
yuucu <yuucu.sub@gmail.com>
2023-12-04 00:57:57 +0900

    Generate a clear and concise git commit message based on the specified instructions.

--- denops/ai-commits/main.ts
+++ denops/ai-commits/main.ts
@@ -1,23 +1,27 @@
 import { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts";
 import { OpenAIChat } from "npm:langchain/llms/openai";
 import { PromptTemplate } from "npm:langchain/prompts";
-export * as path from "https://deno.land/std@0.117.0/path/mod.ts";
 
 // NOTE: export OPENAI_API_KEY="YOUR_OPENAI_API_KEY"
 const llm = new OpenAIChat({});
 
 const promptTemplate = new PromptTemplate({
-  inputVariables: ["input"],
+  inputVariables: ["input", "locale"],
   template: `
-以下のGitDiffを元にcommitMessageを生成してください。
+以下の指定された仕様を元に、明瞭で簡潔なgit commitメッセージを生成します。
+不必要な翻訳や余計な情報は除外して、gitコミットに直接使える形で提供します。
 
-### GitDiff
+### Message Language
+{locale}
+
+### diff
 {input}
 `,
 });
 
 async function sendChatMessage(text: string) {
-  const prompt = await promptTemplate.format({ input: text });
+  const prompt = await promptTemplate.format({ input: text, locale: "ja" });
+  console.log(prompt);
   const commitMessage = await llm.call(prompt);
   return commitMessage;
 }
