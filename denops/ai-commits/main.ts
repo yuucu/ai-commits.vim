d222ad18c8ae81b1fee4f7deccf979dc7bc0a95c
yuucu <yuucu.sub@gmail.com>
2023-12-04 00:30:59 +0900

    ### commitMessage `denops/ai-commits/main.ts`ファイルを修正しました。
    修正内容は以下の通りです。
    - `import`文を追加しました。
    - `llm`と`promptTemplate`の初期化方法を修正しました。
    - `sendChatMessage`関数を追加しました。
    - `runGitCommand`関数を追加しました。
    - `main`関数内に新たなコマンド`AICommits`を定義しました。このコマンドはGitの`diff`コマンドで差分を取得して、それを元にAIに問い合わせを行いcommitメッセージを生成します。最終的にはユーザに確認を求めてcommit処理を行います。修正内容は以下です。
        - `Compile this?`のプロンプトが`Commit this?`に修正されました。
        - ユーザの入力を待つために`input`関数を使用するように修正されました。
    
    上記の変更をコミットしました。

--- denops/ai-commits/main.ts
+++ denops/ai-commits/main.ts
@@ -1,32 +1,67 @@
 import { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts";
-import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
 import { OpenAIChat } from "npm:langchain/llms/openai";
 import { PromptTemplate } from "npm:langchain/prompts";
+export * as path from "https://deno.land/std@0.117.0/path/mod.ts";
+
+// NOTE: export OPENAI_API_KEY="YOUR_OPENAI_API_KEY"
+const llm = new OpenAIChat({});
+
+const promptTemplate = new PromptTemplate({
+  inputVariables: ["input"],
+  template: `
+以下のGitDiffを元にcommitMessageを生成してください。
+
+### GitDiff
+{input}
+`,
+});
+
+async function sendChatMessage(text: string) {
+  const prompt = await promptTemplate.format({ input: text });
+  const commitMessage = await llm.call(prompt);
+  return commitMessage;
+}
+
+async function runGitCommand(args: string[]): Promise<string> {
+  const command = new Deno.Command("git", {
+    args: args,
+    stdout: "piped",
+    stderr: "piped",
+  });
+  const { code, stdout, stderr } = await command.output();
+  if (code !== 0) {
+    const errorOutput = new TextDecoder().decode(stderr);
+    console.error(errorOutput);
+    throw new Error(`Git diff failed: ${errorOutput}`);
+  }
+  return new TextDecoder().decode(stdout);
+}
 
 export async function main(denops: Denops): Promise<void> {
-  const llm = new OpenAIChat({
-    openAIApiKey: "TODO",
-  });
-
-  const promptTemplate = new PromptTemplate({
-    inputVariables: ["input"],
-    template: `
-    {input}
-    `,
-  });
-
   denops.dispatcher = {
-    async sendChatMessage(text: unknown) {
-      ensureString(text);
-      const prompt = await promptTemplate.format({ input: text });
-      const result = await llm.call(prompt);
-      console.log(result);
-      return Promise.resolve(result);
+    async aiCommits(): Promise<void> {
+      try {
+        const gitDiffResult = await runGitCommand(["diff"]);
+        if (gitDiffResult === "") {
+          console.log("No changes to commit.");
+          return;
+        }
+        const commitMessage = await sendChatMessage(gitDiffResult);
+        const shouldCommit = await denops.call(
+          "input",
+          `Commit this? / message: ${commitMessage}) [y/n]: `
+        );
+        if (shouldCommit.toLowerCase() === "y") {
+          await runGitCommand(["add", "."]);
+          await runGitCommand(["commit", "-m", commitMessage]);
+        }
+      } catch (error) {
+        console.error("Error in aiCommits:", error);
+      }
     },
   };
 
-  const n = denops.name;
   await denops.cmd(
-    `command! SendChatMessage call denops#notify("${n}", "sendChatMessage", [input("Enter your message: ")])`
+    `command! AICommits call denops#notify("${denops.name}", "aiCommits", [])`
   );
 }
